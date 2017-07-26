dfa9bcb67d4c281cf69dbc6e523de07217ea3817
hvvivek <hvvivek2007@gmail.com>
2017-07-27 00:49:52 +0530

    Integrated with GetForm - CSS remaining

--- js/script.js
+++ js/script.js
@@ -1,11 +1,40 @@
-window.onload = addListeners;
+window.onload = setVariables
 
-// Global Variables
-var prevX;
-var prevY;
-var handler;
+var questions;
+var screenHeight;
 
-// Code for Draggable Pane
-function addListeners()
+function setVariables()
 {
+    questions = document.querySelectorAll("#form-wrapper>form>ul>li");
+    screenHeight = window.innerHeight;
+}
+
+function checkActiveQuestion()
+{
+    var activeQuestion = questions[0];
+    for( var index = 0; index<questions.length; index = index + 1 )
+    {
+        if( questions[index].classList.contains("active") )
+            {
+                questions[index].classList.remove("active");
+                break;
+            }
+    }
+
+    for( var index = 0; index<questions.length; index = index + 1 )
+    {
+        var bottomPosition = questions[index].getBoundingClientRect().bottom;
+        var topPosition = questions[index].getBoundingClientRect().top;
+        if( bottomPosition >  screenHeight/2-100 && topPosition < screenHeight/2-100)
+        {
+            activeQuestion = questions[index];
+            break;
+        }
+        else if( bottomPosition >  screenHeight/2-100 && topPosition > screenHeight/2-100 )
+        {
+            activeQuestion = questions[index];
+            break;
+        }
+    }
+        activeQuestion.classList.add("active");
 }
