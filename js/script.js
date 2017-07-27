2400653ebdc929ec3037f5e0e69ca1e1eb14add0
hvvivek <hvvivek2007@gmail.com>
2017-07-27 09:32:54 +0530

    css 90pc

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ window.onload = setVariables
 
 var questions;
 var screenHeight;
+var removeBorder;
+var addBorder;
 
 function setVariables()
 {
@@ -39,8 +41,69 @@ function checkActiveQuestion()
         activeQuestion.classList.add("active");
 }
 
+function checkRadioOption(checkOption)
+{
+    console.log("Value changed");
+    var otherQuestions = checkOption.parentNode.parentNode.parentNode.parentNode.querySelectorAll(".multiple-choice");
+        console.log(otherQuestions);
+    for( var index = 0; index < otherQuestions.length; index++ )
+        {
+            if( otherQuestions[index].classList.contains("checked") )
+                {
+                    otherQuestions[index].classList.remove("checked");
+                    break;
+                }
+        }
+
+    checkOption.parentNode.parentNode.parentNode.classList.add("wrapper-no-margin");        
+    checkOption.parentNode.parentNode.classList.add("checked");
+    
+    blink(checkOption);
+    setTimeout(function() {
+        moveToNextQuestion( checkOption );   
+    }, 800, false);
+    
+}
+
 function checkOption(checkOption)
 {
-    console.log(checkOption);
+    console.log("Value changed");
+    checkOption.parentNode.parentNode.parentNode.classList.add("wrapper-no-margin");        
+    checkOption.parentNode.parentNode.classList.add("checked");
     
+    blink(checkOption);
+}
+
+function blink(checkOption)
+{
+    setTimeout(function() {
+        checkOption.parentNode.parentNode.classList.add("no-border");
+        checkOption.parentNode.parentNode.parentNode.classList.remove("wrapper-no-margin");        
+    }, 150, false);
+    setTimeout(function() {
+        checkOption.parentNode.parentNode.classList.remove("no-border");
+        checkOption.parentNode.parentNode.parentNode.classList.add("wrapper-no-margin");        
+
+    }, 300, false);
+    setTimeout(function() {
+        checkOption.parentNode.parentNode.classList.add("no-border");
+        checkOption.parentNode.parentNode.parentNode.classList.remove("wrapper-no-margin");        
+
+    }, 450, false);
+    setTimeout(function() {
+        checkOption.parentNode.parentNode.classList.remove("no-border");
+        checkOption.parentNode.parentNode.parentNode.classList.add("wrapper-no-margin");     
+    }, 600, false);
+    
+}
+
+function moveToNextQuestion(checkOption)
+{
+    console.log(checkOption.parentNode.parentNode.parentNode.parentNode.scrollHeight);
+    scrollBy(0, checkOption.parentNode.parentNode.parentNode.parentNode.scrollHeight+100);
+}
+
+function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight)+"px";
 }
